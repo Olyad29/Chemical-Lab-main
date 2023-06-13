98aafd9e4e68949d4fae447c58a7e6ee75fde71e
Olyad29 <107053697+Olyad29@users.noreply.github.com>
2023-06-13 22:06:56 +0400

    Initial commit

---
+++ Assets/ObjectVisibility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVisibility : MonoBehaviour
+{
+    public GameObject objectToToggle;
+
+    public void ToggleActiveStatus()
+    {
+        if (objectToToggle.activeSelf)
+        {
+            objectToToggle.SetActive(false);
+        }
+        else
+        {
+            objectToToggle.SetActive(true);
+        }
+    }
+}
